5874dd9d855b2fa3e513bcf4f0d1f82e026ae7c8
Hansen Lam <53011466+hansenlam@users.noreply.github.com>
2022-10-03 09:45:00 -0400

    Update case_dictionary_methods.py

--- case_dictionary_methods.py
+++ case_dictionary_methods.py
@@ -1,27 +1,27 @@
-/*============================================================================================
-    AGPL v3.0 NOTICE
-    PLEASE INCLUDE AT START OF ALL SOURCE CODE FILES PERTAINING TO THIS PROGRAM
+# ============================================================================================
+    # AGPL v3.0 NOTICE
+    # PLEASE INCLUDE AT START OF ALL SOURCE CODE FILES PERTAINING TO THIS PROGRAM
 
-    PROGRAM TITLE: PathReporter
-    Copyright (C) 2022  Hansen Lam, M.D., Freddy Nguyen, M.D., Ph.D., Karl Eichorn, Dan Wild
-    Disclaimer: This software is intended for research purposes only. This software is not
-    for clinical use. The authors and contributors shall not be liable for any damages or
-    losses resulting from the use of this software. The authors and contributors shall not 
-    be liable for any actions or decisions resulting rom the use of this software.
+    # PROGRAM TITLE: PathReporter
+    # Copyright (C) 2022  Hansen Lam, M.D., Freddy Nguyen, M.D., Ph.D., Karl Eichorn, Dan Wild
+    # Disclaimer: This software is intended for research purposes only. This software is not
+    # for clinical use. The authors and contributors shall not be liable for any damages or
+    # losses resulting from the use of this software. The authors and contributors shall not 
+    # be liable for any actions or decisions resulting rom the use of this software.
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as published
-    by the Free Software Foundation, either version 3 of the License, or
-    any later version.
+    # This program is free software: you can redistribute it and/or modify
+    # it under the terms of the GNU Affero General Public License as published
+    # by the Free Software Foundation, either version 3 of the License, or
+    # any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
+    # This program is distributed in the hope that it will be useful,
+    # but WITHOUT ANY WARRANTY; without even the implied warranty of
+    # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    # GNU Affero General Public License for more details.
 
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-============================================================================================*/
+    # You should have received a copy of the GNU Affero General Public License
+    # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# ============================================================================================
 
 from collections import defaultdict
 import re
